68ba8b071e19d7f5774d9b3879c4e614fc91ff3e
Arjun pg <arjunpg1@outlook.com>
2022-06-04 22:52:10 +0530

    edit institute

--- src/modules/Institute/containers/EditInstitute.js
+++ src/modules/Institute/containers/EditInstitute.js
@@ -1,40 +1,23 @@
 import { ContentLayout } from "shared/components";
 import { Input, Button } from "reactstrap";
-import Datetime from "react-datetime";
 import React from "react";
 import { useParams } from "react-router-dom";
-import moment from "moment";
 
 export const EditInstitute = (props) => {
-  const { batch, setBatch, editBatch, onEdit } = props;
-  const { name, startDate, endDate } = batch;
-  const endDateVal = moment(endDate, "YYYY-MM-DD");
-  const startDateVal = moment(startDate, "YYYY-MM-DD");
+  const { institute, setInstitute, editInstitute, onEdit } = props;
 
-  let { batchId } = useParams();
+  const { name } = institute;
+
+  let { instituteId } = useParams();
 
   React.useEffect(() => {
-    if (batchId) {
-      onEdit(parseInt(batchId.toString(), 10));
+    if (instituteId) {
+      onEdit(parseInt(instituteId.toString(), 10));
     }
-  }, [batchId, onEdit]);
-
-  const onStartDateChange = (m) => {
-    const date = m.format("YYYY-MM-DD").toString();
-    setBatch((draft) => {
-      draft.startDate = date;
-    });
-  };
-
-  const onEndDateChange = (m) => {
-    const date = m.format("YYYY-MM-DD").toString();
-    setBatch((draft) => {
-      draft.endDate = date;
-    });
-  };
+  }, [instituteId, onEdit]);
 
   const onSubmit = () => {
-    editBatch.mutate(batch);
+    editInstitute.mutate(institute);
   };
 
   return (
@@ -59,36 +42,10 @@ export const EditInstitute = (props) => {
                               placeholder="Batch Name"
                               value={name}
                               onChange={(e) => {
-                                setBatch((draft) => {
+                                setInstitute((draft) => {
                                   draft.name = e.target.value;
                                 });
                               }}
-                            />
-                          </div>
-                        </div>
-                        <div className="col-12">
-                          <div className="form-group">
-                            <label htmlFor="contact-info-vertical">Start Date</label>
-                            <Datetime
-                              dateformat="YYYY-MM-DD"
-                              timeformat="{false}"
-                              name="startDate"
-                              selected={startDateVal}
-                              value={startDateVal}
-                              onChange={onStartDateChange}
-                            />
-                          </div>
-                        </div>
-                        <div className="col-12">
-                          <div className="form-group">
-                            <label htmlFor="contact-info-vertical">End Date</label>
-                            <Datetime
-                              dateformat="YYYY-MM-DD"
-                              timeformat="{false}"
-                              name="endDate"
-                              selected={endDateVal}
-                              value={endDateVal}
-                              onChange={onEndDateChange}
                             />
                           </div>
                         </div>
