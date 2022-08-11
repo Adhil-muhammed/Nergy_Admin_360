01af6f4fd8a78631b0ae4404a5b956bbd6d90187
arunraj.r <arunraj.r@nergyindia.com>
2022-08-11 11:48:39 +0530

    minor fix

--- src/index.js
+++ src/index.js
@@ -15,15 +15,15 @@ import "react-datetime/css/react-datetime.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    {/* <React.StrictMode> */}
-    <meta charSet="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap"
-      rel="stylesheet"
-    />
-    <Master />
-    {/* </React.StrictMode> */}
+    <React.StrictMode>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap"
+        rel="stylesheet"
+      />
+      <Master />
+    </React.StrictMode>
   </>
 );
 
