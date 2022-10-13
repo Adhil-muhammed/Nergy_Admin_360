f7ee35e5837810b7e5ebdd3bb15e4054594f7e45
bijothomas <studybijo@gmail.com>
2022-10-13 13:55:36 +0530

    course content update

--- src/modules/Courses/hooks/useCourseContent.js
+++ src/modules/Courses/hooks/useCourseContent.js
@@ -2,28 +2,38 @@ import React, { useEffect } from "react";
 import { useQuery, useMutation, useQueryClient } from "react-query";
 import { useImmer } from "use-immer";
 import {
-    createCoursesContent,
-    deleteCoursesContentById,
-    updateCoursesContentById,
-    getCourseContentById,
-} from '../api';
+  createCoursesContent,
+  deleteCoursesContentById,
+  updateCoursesContentById,
+  getCourseContentById,
+} from "../api";
 import { useNavigate } from "react-router-dom";
 import { successMessage, successDeletedMessage, errorMessage } from "utils";
 
 const GET_COURSECONTENT = "GET_COURSECONTENT";
 const GET_COURSECONTENT_BY_ID = "GET_COURSECONTENT_BY_ID";
 
-export const useCourse = ({ load = false, courseContentId = 0 }) => {
-    const createCourseContent = useMutation(createCoursesContent, {
-        onError: (e, newData, previousData) => {
-        errorMessage("Unable to create!");
-        },
-        onSuccess: () => {
-        successMessage();
-        queryClient.invalidateQueries(`${GET_COURSECONTENT_BY_ID}_${courseContentId}`);
-        },
-        onSettled: () => {
-        setIsModalOpen(false);
+export const useCourseContent = ({ load = false, contentId = 0 }) => {
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
+  const [isConfirmDelete, setIsConfirmDelete] = useImmer(false);
+  const [courseContent, setCourseContent] = useImmer({
+    contentId: 0,
+    title: "",
+    fileName: "",
+    fileURL: "",
+    isExternal: false,
+    isVideo: false,
+  });
+  const createCourseContent = useMutation(createCoursesContent, {
+    onError: (e, newData, previousData) => {
+      errorMessage("Unable to create!");
+    },
+    onSuccess: () => {
+      successMessage();
+      queryClient.invalidateQueries(GET_COURSECONTENT);
+      navigate("..", { replace: true });
     },
   });
 
@@ -33,7 +43,7 @@ export const useCourse = ({ load = false, courseContentId = 0 }) => {
     },
     onSuccess: () => {
       successDeletedMessage();
-      queryClient.invalidateQueries(`${GET_COURSECONTENT_BY_ID}_${courseContentId}`);
+      queryClient.invalidateQueries(`${GET_COURSECONTENT_BY_ID}_${contentId}`);
     },
     onSettled: () => {
       onToggleModal(false);
@@ -51,13 +61,45 @@ export const useCourse = ({ load = false, courseContentId = 0 }) => {
     },
   });
 
-  const assessmentSectionInfo = useQuery(
-    `${GET_ASSESSMENTSECTIONS_BY_ID}_${courseContentId}`,
-    () => getCourseContentById(courseContentId),
+  const courseCotentInfo = useQuery(
+    `${GET_COURSECONTENT_BY_ID}_${contentId}`,
+    () => getCourseContentById(contentId),
     {
       refetchOnWindowFocus: false,
-      enabled: sectionId > 0,
+      enabled: contentId > 0,
     }
-  ); 
+  );
 
-}
+  const onDelete = React.useCallback(
+    (id) => {
+      setIsConfirmDelete((draft) => {
+        draft = true;
+        return draft;
+      });
+      setCourseContent((draft) => {
+        draft.contentId = id;
+        return draft;
+      });
+    },
+    [setIsConfirmDelete]
+  );
+
+  const onToggleModal = React.useCallback(
+    (isOpen) => {
+      setIsConfirmDelete((draft) => {
+        draft = isOpen;
+        return draft;
+      });
+    },
+    [setIsConfirmDelete]
+  );
+
+  return {
+    deleteCourseContent,
+    isConfirmDelete,
+    courseContent,
+    onToggleModal,
+    courseCotentInfo,
+    onDelete,
+  };
+};
