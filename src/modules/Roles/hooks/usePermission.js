e1877e5aa3cb774068c6a5590133e9687f8ca151
arjun.pg <arjun.pg@nergyindia.com>
2022-10-25 10:14:01 +0530

    remove unused code

--- src/modules/Roles/hooks/usePermission.js
+++ src/modules/Roles/hooks/usePermission.js
@@ -4,21 +4,11 @@ import { updateModulePermissions, getModulePermissions } from "..";
 import { successMessage, errorMessage } from "utils";
 import { useImmer } from "use-immer";
 
-
 const GET_PERMISSIONS = "GET_PERMISSIONS";
-
-export const action_types = {
-  View: "View",
-  Create: "Create",
-  Edit: "Edit",
-  Delete: "Delete",
-};
 
 export const usePermission = ({ userRole = "" }) => {
   const queryClient = useQueryClient();
   const [modulePermissions, setmodulePermissions] = useImmer([]);
-
-
 
   const permissionQuery = useQuery(
     `${GET_PERMISSIONS}_${userRole}`,
@@ -51,32 +41,6 @@ export const usePermission = ({ userRole = "" }) => {
     onSettled: () => {
     },
   });
-
-  // const rolesQuery = useQuery(GET_ROLES, async () => await getRoles(), {
-  //   refetchOnWindowFocus: false,
-  //   enabled: load,
-  //   staleTime: Infinity,
-  // });
-
-  // const hasPermission = (module, action) => {
-  //   if (permissionQuery.isSuccess && permissionQuery.data) {
-  //     let permissionListLength = permissionQuery.data.length;
-  //     for (let j = 0; j < permissionListLength; j++) {
-  //       if (permissionQuery.data[j].name === module) {
-  //         for (let i = 0; i < permissionQuery.data[j].actions.length; i++) {
-  //           let action_name = permissionQuery.data[j].actions[i].name.split(".").at(-1).toString();
-  //           if (action_name === action) {
-  //             return permissionQuery.data[j].actions[i].allowed;
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
-  //   return false;
-  // };
-
-
-
 
   return {
     permissionQuery,
