7066cfc63453e6029c01e65c32ca878229f6e83b
bijothomas <studybijo@gmail.com>
2022-10-28 11:08:20 +0530

    thumbnail

--- src/modules/Courses/containers/ContentList.js
+++ src/modules/Courses/containers/ContentList.js
@@ -39,57 +39,14 @@ export const ContentList = (props) => {
                     )}
                   </td>
                   <td>
-                    {content.fileURL && content.contentType == 0 ? (
-                      //display pdf icon
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1"
-                        stroke="currentColor"
-                        class="w-1 h-1"
-                        width="20%"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
-                        />
-                      </svg>
-                    ) : content.fileURL &&
-                      (content.contentType == 1 || content.contentType == 2) ? (
-                      // display video icon
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1"
-                        stroke="currentColor"
-                        class="w-1 h-1"
-                        width="20%"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z"
-                        />
-                      </svg>
-                    ) : content.fileURL && content.contentType == 3 ? (
-                      //display pdf icon
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        class="w-1 h-1"
-                        width="20%"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
-                        />
-                      </svg>
+                    {content.fileURL && content.contentType === 0 ? (
+                      <img src={content.fileURL} style={{ height: "40px" }} />
+                    ) : content.fileURL && content.contentType === 1 ? (
+                      <source src={content.fileURL} type="video/mp4"></source>
+                    ) : content.fileURL && content.contentType === 2 ? (
+                      <iframe width="70" height="70" frameborder="0" src={content.fileURL}></iframe>
+                    ) : content.fileURL && content.contentType === 3 ? (
+                      <iframe width="70" height="70" frameborder="0" src={content.fileURL}></iframe>
                     ) : (
                       <span>No files</span>
                     )}
