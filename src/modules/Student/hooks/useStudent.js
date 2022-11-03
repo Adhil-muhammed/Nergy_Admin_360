67712650ebf012267d75a25df31acaba485cb7e4
bijothomas <studybijo@gmail.com>
2022-11-03 12:56:09 +0530

    batches and institutes are displayed on create student without refresh

--- src/modules/Student/hooks/useStudent.js
+++ src/modules/Student/hooks/useStudent.js
@@ -34,8 +34,8 @@ export const useStudent = ({ load = false, studentId = 0 }) => {
     enabled: load,
     staleTime: Infinity,
   });
-  const batchesQuery = useQuery(GetBatchKey, getBatches, { staleTime: Infinity });
-  const institutesQuery = useQuery(GetInstituteKey, getInstitutes, { staleTime: Infinity });
+  const batchesQuery = useQuery(GetBatchKey, getBatches, { staleTime: 0 });
+  const institutesQuery = useQuery(GetInstituteKey, getInstitutes, { staleTime: 0 });
   // const coursesQuery = useQuery(GetCourseKey, getCourses, { staleTime: Infinity });
 
   // const courses = React.useMemo(() => {
